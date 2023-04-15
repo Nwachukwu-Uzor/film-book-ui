a5757bb1bc8b5915bff9f4e35fddd0a1d2ed01c1
Nwachukwu-Uzor <uzor1997@gmail.com>
2023-04-15 10:24:30 +0100

    makes google signup and signin functional in development environment

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -16,14 +16,14 @@ interface Props {
   baseUrl: string;
 }
 
-interface UserRegisterationResponse {
+interface UserSigninationResponse {
   data: {
     message: string;
+    token: string;
   };
 }
 
 const initialFieldValues = {
-  username: "",
   password: "",
   email: "",
 };
@@ -32,69 +32,34 @@ const validationSchema = Yup.object().shape({
   username: Yup.string()
     .required("Username is required")
     .min(5, "Username should be at least 5 characters"),
-  email: Yup.string()
-    .required("Email is required")
-    .email("Please provide a valid email address"),
   password: Yup.string()
     .required("Password is required")
     .min(5, "Password should be at least 5 characters"),
 });
 
-const Register: FC<Props> = ({ baseUrl }) => {
+const Signin: FC<Props> = ({ baseUrl }) => {
   const router = useRouter();
 
-  const [avatar, setAvatar] = useState<File | null | undefined>(null);
-  const [preview, setPreview] = useState<string | null | undefined>(null);
-
-  const handleGoogleSigup = () => {
-    window.open(`${baseUrl}/auth/google/signup`, "_self");
+  const handleGoogleSignin = () => {
+    window.open(`${baseUrl}/auth/google/login`, "_self");
   };
 
-  const handleFacebookSignup = () => {
+  const handleFacebookSignin = () => {
     window.open(`${baseUrl}/auth/facebook`, "_self");
-  };
-
-  const handleAvatarChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event?.target?.files?.[0];
-    if (!file) {
-      return;
-    }
-    console.log("here");
-    setAvatar(file);
   };
 
   const { values, handleSubmit, handleChange, touched, errors, isSubmitting } =
     useFormik({
       initialValues: initialFieldValues,
       validationSchema: validationSchema,
-      onSubmit: async (value, { resetForm }) => {
-        if (!avatar) {
-          Swal.fire({
-            title: "Error",
-            text: "Please provide an avatar image",
-          });
-        }
+      onSubmit: async (values, { resetForm }) => {
         try {
-          const formData = new FormData();
-          formData.append("email", value.email);
-          formData.append("password", value.password);
-          formData.append("username", value.username);
-
-          const response = await axios.post<UserRegisterationResponse>(
-            `${baseUrl}/auth/register`,
-            formData
+          const response = await axios.post<UserSigninationResponse>(
+            `${baseUrl}/auth/signin`,
+            values
           );
-          if (response.status === 201) {
-            Swal.fire({
-              title: "Registeration Successful",
-              text: response?.data?.data?.message,
-              didClose: () => {
-                router.push(`/register/success?email=${value?.email}`);
-              },
-            });
-            setAvatar(null);
-            resetForm();
-          }
+
+          router.push(`/`);
         } catch (error: any) {
           const message = error?.response?.data?.message ?? error?.message;
           Swal.fire({
@@ -105,31 +70,15 @@ const Register: FC<Props> = ({ baseUrl }) => {
       },
     });
 
-  // create a preview as a side effect, whenever selected file is changed
-  useEffect(() => {
-    if (!avatar) {
-      setPreview(undefined);
-      return;
-    }
-
-    const objectUrl = URL.createObjectURL(avatar);
-    console.log(objectUrl);
-    setPreview(objectUrl);
-
-    // free memory when ever this component is unmounted
-    return () => URL.revokeObjectURL(objectUrl);
-  }, [avatar]);
-
   return (
     <section className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="grid grid-cols-1 lg:grid-cols-2 w-[95%] max-w-[1500px]">
         <div className="relative hidden lg:block m-1">
           <Image
             src="/images/signup-bg.png"
-            alt="register Image"
+            alt="signin Image"
             width="600"
             height="600"
-            // fill={true}
             className="w-full h-screen object-cover border shadow-sm rounded-sm"
           />
           <div className="absolute left-0 right-0 bottom-8 flex flex-col items-center text-white gap-2 p-4">
@@ -149,28 +98,17 @@ const Register: FC<Props> = ({ baseUrl }) => {
             </h1>
             <div className="flex items-center gap-2">
               <SocialSigninButton
-                title="Sign up with Google"
-                handleClick={handleGoogleSigup}
+                title="Login with Google"
+                handleClick={handleGoogleSignin}
                 imageUrl="/images/google.png"
               />
               <SocialSigninButton
-                title="Sign up with Facebook"
-                handleClick={handleFacebookSignup}
+                title="Login with Facebook"
+                handleClick={handleFacebookSignin}
                 imageUrl="/images/facebook.png"
               />
             </div>
             <form className="my-2 flex flex-col gap-2" onSubmit={handleSubmit}>
-              <TextInput
-                value={values?.username}
-                handleChange={handleChange}
-                touched={Boolean(touched?.username)}
-                isError={Boolean(errors?.username) ?? false}
-                name="username"
-                label="Username"
-                error={errors?.username ?? ""}
-                id="username"
-                type="text"
-              />
               <TextInput
                 value={values?.email}
                 handleChange={handleChange}
@@ -193,34 +131,6 @@ const Register: FC<Props> = ({ baseUrl }) => {
                 id="password"
                 type="password"
               />
-              <div className="my-2 flex justify-between">
-                <label
-                  htmlFor="avatar"
-                  className="flex flex-col gap-2 items-center w-fit lg:flex-row cursor-pointer"
-                >
-                  <span className="font-bold">Select Avatar: </span>
-                  <span className="h-[40px] w-[40px] bg-yellow-100 rounded-full flex items-center justify-center">
-                    <AiFillFileImage className="text-yellow-500" />
-                  </span>
-                </label>
-                <input
-                  type="file"
-                  accept=".jpg,.png,.jpeg"
-                  hidden
-                  name="avatar"
-                  id="avatar"
-                  onChange={handleAvatarChange}
-                />
-                {preview ? (
-                  <Image
-                    src={preview}
-                    alt="avatar"
-                    height="300"
-                    width="300"
-                    className="h-[50px] w-[50px] object-cover rounded-full"
-                  />
-                ) : null}
-              </div>
               {isSubmitting ? (
                 <p className="">
                   <svg
@@ -230,7 +140,7 @@ const Register: FC<Props> = ({ baseUrl }) => {
                   Submitting
                 </p>
               ) : (
-                <Button title="Submit" type="submit" />
+                <Button title="Sign in" type="submit" />
               )}
             </form>
           </div>
@@ -253,4 +163,4 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
   };
 };
 
-export default Register;
+export default Signin;
